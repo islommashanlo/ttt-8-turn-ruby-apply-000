24fd7b094bb704a5973f21342f54f94d18d60a83
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-14 17:37:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,21 @@
-def display_board(array, index, value)
-array[index] = value
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = [" ", " ", " "," ", " ", " "," ", " ", " "]
+def input_to_index(user_input)
+  converted_input = "#{user_input}".to_i
+  converted_input -= 1
+  return converted_input
+end
 
-
+def move(array, index, value = "X")
+  array[index] = value
+  return array
+end
 #position_taken
   def position_taken?(board, index)
     if board[index] == " " or board[index] == "" or board[index] == nil
